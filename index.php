062f8c38b433475bbc75f14846fae9cf8a8b1c80
David Nanjila <nanjiladavid2@gmail.com>
2024-06-27 16:12:41 +0300

    Feat: Create a form to capture user feedback and save it to the database

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+include 'db.php';
+$name = $email = $feedback = $rate = "";
+if (isset($_POST['submit'])){
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $feedback = $_POST['feedback'];
+    $rate = $_POST['rate'];
+    $sql = "INSERT INTO feedback (name, email, feedback, rating) VALUES ('$name', '$email', '$feedback', '$rate')";
+    if ($connection->query($sql) === TRUE){
+        echo "Feedback submitted successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $connection->error;
+    }
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -8,6 +24,35 @@
     <title>Home</title>
 </head>
 <body>
-    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="view.php">Rates</a></li>
+            <li><a href=".php"></a></li>
+        </ul>
+        <section>
+            <div class="container">
+                <form action="" method="post">
+                    <div class="mb-3">
+                        <label for="name" class="form-label">Name</label>
+                        <input type="text" class="form-control" id="name" name="name">
+                    </div>
+                    <div class="mb-3">
+                        <label for="email" class="form-label">Email</label>
+                        <input type="email" class="form-control" id="email" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label for="feeback" class="form-label">Feedback</label>
+                        <textarea class="form-control" id="feedback" name="feedback"></textarea>
+                    </div>
+                    <div class="mb-3">
+                        <label for="rate" class="form-label">Rate</label>
+                        <input type="number" class="form-control" id="rate" name="rate">
+                    </div>
+                    <button type="submit" class="btn btn-primary" name="submit">Submit</button>
+                </form>
+            </div>
+        </section>
+    </nav>
 </body>
 </html>
